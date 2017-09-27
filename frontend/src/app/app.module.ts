adbde7bfa5119e5abe0a6cf7b3c6efb86b7c5a3d
Jacek Stachowiak <j.stachowiak2@ktr.local>
2017-09-27 20:51:11 +0200

    Poprawka 2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { CookieLawBannerComponent } from './cookie-law-banner/cookie-law-banner.component';
 import { AboutUsComponent } from './static/about-us.component';
+import { OfficeComponent } from './static/office.component';
 
 const appRoutes: Routes = [
    // {
@@ -23,6 +24,10 @@ const appRoutes: Routes = [
   {
     path: 'o-nas',
     component: AboutUsComponent
+  },
+  {
+    path: 'office',
+    component: OfficeComponent
   },
   {
     path: '**',
@@ -38,7 +43,8 @@ const appRoutes: Routes = [
     HeaderComponent,
     FooterComponent,
     CookieLawBannerComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    OfficeComponent,
   ],
   imports: [
     BrowserModule,
